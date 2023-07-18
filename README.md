4b8b33c59f996f134d8994d3a3e93274b7374532
Daffa Liefalza <liefalzaa@gmail.com>
2023-07-18 07:27:15 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+<a href="https://daffaliefalza.github.io/zonama">Live Site</a>
---
+++ assets/data.js
@@ -0,0 +1,74 @@
+export const data = [
+  {
+    id: 1,
+    title:
+      "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
+    image:
+      "https://m.media-amazon.com/images/I/51B7kuFwQFL._SX329_BO1,204,203,200_.jpg",
+    price: 59,
+  },
+
+  {
+    id: 2,
+    title: "America's Cultural Revolution",
+    image:
+      "https://m.media-amazon.com/images/I/51mN4alPnKL._SX327_BO1,204,203,200_.jpg",
+    price: 40,
+  },
+
+  {
+    id: 3,
+    title: "American Prometheus",
+    image:
+      "https://m.media-amazon.com/images/I/51rBwNT0gEL._SX321_BO1,204,203,200_.jpg",
+    price: 20,
+  },
+  {
+    id: 1,
+    title:
+      "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
+    image:
+      "https://m.media-amazon.com/images/I/51B7kuFwQFL._SX329_BO1,204,203,200_.jpg",
+    price: 59,
+  },
+
+  {
+    id: 2,
+    title: "America's Cultural Revolution",
+    image:
+      "https://m.media-amazon.com/images/I/51mN4alPnKL._SX327_BO1,204,203,200_.jpg",
+    price: 40,
+  },
+
+  {
+    id: 3,
+    title: "American Prometheus",
+    image:
+      "https://m.media-amazon.com/images/I/51rBwNT0gEL._SX321_BO1,204,203,200_.jpg",
+    price: 20,
+  },
+  {
+    id: 1,
+    title:
+      "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
+    image:
+      "https://m.media-amazon.com/images/I/51B7kuFwQFL._SX329_BO1,204,203,200_.jpg",
+    price: 59,
+  },
+
+  {
+    id: 2,
+    title: "America's Cultural Revolution",
+    image:
+      "https://m.media-amazon.com/images/I/51mN4alPnKL._SX327_BO1,204,203,200_.jpg",
+    price: 40,
+  },
+
+  {
+    id: 3,
+    title: "American Prometheus",
+    image:
+      "https://m.media-amazon.com/images/I/51rBwNT0gEL._SX321_BO1,204,203,200_.jpg",
+    price: 20,
+  },
+];
